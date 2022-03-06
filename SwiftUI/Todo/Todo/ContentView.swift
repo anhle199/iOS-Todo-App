3a7cd8de7c4415757a591db1346953c5787b151e
Le Hoang Anh <lehoanganh.le2001@gmail.com>
2022-03-06 23:34:34 +0700

    SwiftUI - initialize SwiftUI project

---
+++ SwiftUI/Todo/Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by Le Hoang Anh on 06/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
